fa6e0b4a3c8aed28974908b340f1b07bc5257367
Hope <45346421+Hopefullyidontgetbanned@users.noreply.github.com>
2024-12-14 15:05:54 -0800

    Add Sphinxawesome pt2.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-	'sphinxawesome_theme',
+    'sphinxawesome_theme',
 ]
 
 intersphinx_mapping = {
